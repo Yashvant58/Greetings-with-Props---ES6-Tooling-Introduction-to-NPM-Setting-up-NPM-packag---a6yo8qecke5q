b8a7f0b70abd28d155fd819e59240fb7d27772fa
Yashvant <prajapatiyashvant746@gmail.com>
2022-12-15 08:42:16 +0530

    Intial commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
-// write code for Welcome component here
+import PropTypes from 'prop-types'
+import React from 'react'
+
+function Welcome(props) {
+  return (
+    <div className='main'>
+        <h1>Hey !{props.name}</h1>
+        <h2>Welcome to Newton School.</h2>
+    </div>
+  )
+}
+
+export default Welcome
